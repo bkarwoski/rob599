677fdc4076749ecd20223ef5fe9ab5d58b78afee
Blake M Karwoski <bkarw@beyster1620p20.engin.umich.edu>
2019-09-10 15:35:38 -0400

    initial error messages

---
+++ class3/parity.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("0\n");
+        return 0;
+    }
+    if (argc > 2) {
+        printf("usage: ./parity [<text>]\n");
+        return 0;
+    }
+
+return 0;
+}
+
