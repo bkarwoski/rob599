d21f5d29f3d813f2f02254fb570d1471336b11b3
Blake M Karwoski <bkarw@beyster1620p20.engin.umich.edu>
2019-09-10 14:49:19 -0400

    actually makes words upper case

--- class3/upper.c
+++ class3/upper.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -7,7 +8,18 @@ int main(int argc, char *argv[])
         printf("usage: ./upper <text>\n");
         return 1;
     }
-    printf("input is: %s\n", argv[1]);
+    //printf("input is: %s\n", argv[1]);
+
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; j < strlen(argv[i]); j++)
+        {
+if(argv[i][j] > 91){
+    argv[i][j] -= 32;
+}
+            printf("%c\n", argv[i][j]);
+        }
+    }
 
     return 0;
 }
