28f3db8a637aa8b2b140b95334494399e94b80d9
Blake M Karwoski <bkarw@beyster1620p20.engin.umich.edu>
2019-09-10 15:03:49 -0400

    indentations

--- class3/upper.c
+++ class3/upper.c
@@ -12,10 +12,10 @@ int main(int argc, char *argv[]) {
         for (int j = 0; j < strlen(argv[i]); j++) {
             if (argv[i][j] > 96 && argv[i][j] < 123) {
                 argv[i][j] -= 32;
-                }
+            }
             printf("%c", argv[i][j]);
         }
     }
-printf("\n");
+    printf("\n");
     return 0;
 }
