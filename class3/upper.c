62e401a0b121989c8205b7d27c2e18a47dac3708
Blake M Karwoski <bkarw@beyster1620p20.engin.umich.edu>
2019-09-10 14:12:34 -0400

    style

--- class3/upper.c
+++ class3/upper.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
-    if(argc==1){
-     //   fprintf("usage: ./upper <text> \n");
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        //   fprintf("usage: ./upper <text> \n");
         return 0;
     }
-printf("input is: %s \n", argv[1]);
+    printf("input is: %s\n", argv[1]);
 
     return 0;
 }
