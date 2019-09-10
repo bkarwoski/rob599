0ff296a0c954ce530da1f30ecea89752cd94472b
Blake M Karwoski <bkarw@beyster1620p20.engin.umich.edu>
2019-09-10 14:58:00 -0400

    update formatting

--- class3/upper.c
+++ class3/upper.c
@@ -3,18 +3,15 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1)
-    {
+    if (argc == 1) {
         printf("usage: ./upper <text>\n");
         return 1;
     }
     //printf("input is: %s\n", argv[1]);
 
-    for (int i = 1; i < argc; i++)
-    {
-        for (int j = 0; j < strlen(argv[i]); j++)
-        {
-if(argv[i][j] > 96 && argv[i][j] < 123){
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; j < strlen(argv[i]); j++) {
+if (argv[i][j] > 96 && argv[i][j] < 123) {
     argv[i][j] -= 32;
 }
             printf("%c", argv[i][j]);
