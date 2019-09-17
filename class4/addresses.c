a5872565f8a3dda225effca9536282c64f07af6c
Blake Karwoski <bkarw@umich.edu>
2019-09-17 14:44:00 -0400

    Agreg

--- class4/addresses.c
+++ class4/addresses.c
@@ -4,27 +4,27 @@ char char1;
 int int1;
 double doubles[3];
 
-int main(void){
+int main(void) {
     char char2;
     int int2;
     float floats[3];
    // int variable = 10;
    // printf("%p\n", (void *)&variable);
    // return 0;
-    printf("%li\n", (void *)&char1);
-    printf("%li\n", (void *)&int1);
-    printf("%li\n", (void *)&char1-(void *)&int1);
-    printf("%li\n", (void *)&doubles);
-    printf("%li\n", (void *)&doubles[0]);
-    printf("%li\n", (void *)&doubles[1]);
-    printf("%li\n", (void *)&doubles[2]);
+    printf("%p\n", (void *)&char1);
+    printf("%p\n", (void *)&int1);
+    printf("%p\n", (long)&char1-(long)&int1);
+    printf("%p\n", (void *)&doubles);
+    printf("%p\n", (void *)&doubles[0]);
+    printf("%p\n", (void *)&doubles[1]);
+    printf("%p\n", (void *)&doubles[2]);
 
-    printf("%li\n", (void *)&char2);
-    printf("%li\n", (void *)&int2);
-    printf("%li\n", (void *)&char2-(void *)&int2);
-    printf("%li\n", (void *)&floats);
-    printf("%li\n", (void *)&floats[0]);
-    printf("%li\n", (void *)&floats[1]);
-    printf("%li\n", (void *)&floats[2]);
+    printf("%p\n", (void *)&char2);
+    printf("%p\n", (void *)&int2);
+    printf("%p\n", (void *)&char2-(void *)&int2);
+    printf("%p\n", (void *)&floats);
+    printf("%p\n", (void *)&floats[0]);
+    printf("%p\n", (void *)&floats[1]);
+    printf("%p\n", (void *)&floats[2]);
 return 0;
 }
