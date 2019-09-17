ff0f9a91d9a9f16566fdbbc310e29998ffa9c2fa
Blake Karwoski <bkarw@umich.edu>
2019-09-17 14:01:26 -0400

    test

--- class4/addresses.c
+++ class4/addresses.c
@@ -2,15 +2,29 @@
 
 char char1;
 int int1;
-
+double doubles[3];
 
 int main(void){
-
+    char char2;
+    int int2;
+    float floats[3];
    // int variable = 10;
    // printf("%p\n", (void *)&variable);
    // return 0;
     printf("%p\n", (void *)&char1);
     printf("%p\n", (void *)&int1);
     printf("%p\n", (void *)&char1-(void *)&int1);
+    printf("%p\n", (void *)&doubles);
+    printf("%p\n", (void *)&doubles[0]);
+    printf("%p\n", (void *)&doubles[1]);
+    printf("%p\n", (void *)&doubles[2]);
+
+    printf("%p\n", (void *)&char2);
+    printf("%p\n", (void *)&int2);
+    printf("%p\n", (void *)&char2-(void *)&int2);
+    printf("%p\n", (void *)&floats);
+    printf("%p\n", (void *)&floats[0]);
+    printf("%p\n", (void *)&floats[1]);
+    printf("%p\n", (void *)&floats[2]);
 return 0;
 }
