c62afda8b2e3a86c86df8f04ccfb5903a68b6fb5
Blake Karwoski <bkarw@umich.edu>
2019-09-17 19:57:14 -0400

    formatting

--- homework1/collision.c
+++ homework1/collision.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 #define M_PI 3.14159265358979323846
-#define DEGTORAD M_PI / 180
+#define DEGTORAD (M_PI / 180)
 
 typedef struct poly {
-
     int numPoints;
     double xPoints[16];
     double yPoints[16];
@@ -19,7 +18,7 @@ void shift(poly_t *polyX) {
     float s = sin(polyX->rot);
     double newX = 0;
     double newY = 0;
-    for(int i = 0; i < polyX->numPoints; i++){
+    for (int i = 0; i < polyX->numPoints; i++){
         newX = polyX->xPoints[i] * c - polyX->yPoints[i] * s;
         newY = polyX->xPoints[i] * s + polyX->yPoints[i] * c;
 
@@ -29,20 +28,18 @@ void shift(poly_t *polyX) {
         polyX->xPoints[i] = newX;
         polyX->yPoints[i] = newY;
     }
-
 }
 
-int main(){
-
+int main(void) {
 char tmp[100];
 poly_t poly1 = { 0 };
 poly_t poly2 = { 0 };
 FILE *fp = fopen("polygons.csv", "r");
-if(fp == NULL) {
+if (!fp) {
     fprintf(stderr, "polygons.csv is missing\n");
     return 1;
 }
-fgets(tmp, 100, fp); //clear first line
+fgets(tmp, 100, fp);
 
 fscanf(fp, "%lf", &poly1.xShift);
 fscanf(fp, "%lf", &poly1.yShift);
@@ -50,14 +47,14 @@ fscanf(fp, "%lf", &poly1.rot);
 poly1.rot *= DEGTORAD;
 fscanf(fp, "%d", &poly1.numPoints);
 int pointCheck = 0;
-for(int i = 0; i < poly1.numPoints; i++) {
+for (int i = 0; i < poly1.numPoints; i++) {
     pointCheck += fscanf(fp, "%lf", &poly1.xPoints[i]);
 }
 
-for(int i = 0; i < poly1.numPoints; i++) {
+for (int i = 0; i < poly1.numPoints; i++) {
     pointCheck += fscanf(fp, "%lf", &poly1.yPoints[i]);
 }
-if(pointCheck != (2 *poly1.numPoints)) {
+if (pointCheck != (2 * poly1.numPoints)) {
     fprintf(stderr, "a polygons.csv number is invalid\n");
     return 1;
 }
@@ -68,18 +65,19 @@ fscanf(fp, "%lf", &poly2.rot);
 poly2.rot *= DEGTORAD;
 fscanf(fp, "%d", &poly2.numPoints);
 pointCheck = 0;
-for(int i = 0; i < poly2.numPoints; i++) {
+for (int i = 0; i < poly2.numPoints; i++) {
     pointCheck += fscanf(fp, "%lf", &poly2.xPoints[i]);
 }
-for(int i = 0; i < poly2.numPoints; i++) {
+for (int i = 0; i < poly2.numPoints; i++) {
     pointCheck += fscanf(fp, "%lf", &poly2.yPoints[i]);
 }
-if(pointCheck != (2 *poly1.numPoints)) {
+if (pointCheck != (2 *poly1.numPoints)) {
     fprintf(stderr, "a polygons.csv number is invalid\n");
     return 1;
 }
 fclose(fp);
-if(poly1.numPoints > 16 || poly2.numPoints > 16) {
+
+if (poly1.numPoints > 16 || poly2.numPoints > 16) {
     printf("too many points!\n");
     return 1;
 }
@@ -87,7 +85,6 @@ if(poly1.numPoints > 16 || poly2.numPoints > 16) {
 shift(&poly1);
 shift(&poly2);
 //printf("%f\n,", poly1.xPoints[2]);
-
 
 return 0;
 }
