4dec2b1eac8467686f33d6b768bf2eae01bbc7c2
Blake Karwoski <bkarw@umich.edu>
2019-09-17 14:49:49 -0400

    Agreg

--- class4/addresses.c
+++ class4/addresses.c
@@ -13,17 +13,17 @@ int main(void) {
    // return 0;
     printf("%p\n", (void *)&char1);
     printf("%p\n", (void *)&int1);
-    printf("%p\n", (long)&char1-(long)&int1);
+    printf("%ld\n", (long)&char1 - (long)&int1);
     printf("%p\n", (void *)&doubles);
     printf("%p\n", (void *)&doubles[0]);
     printf("%p\n", (void *)&doubles[1]);
     printf("%p\n", (void *)&doubles[2]);
-    printf("%p\n", doubles[1]-doubles[0]);
-    printf("%p\n", (long)doubles[1]-(long)doubles[0]);
+    printf("%p\n", doubles[1] - doubles[0]);
+    printf("%ld\n", (long)doubles[1] - (long)doubles[0]);
 
     printf("%p\n", (void *)&char2);
     printf("%p\n", (void *)&int2);
-    printf("%p\n", (void *)&char2-(void *)&int2);
+    printf("%p\n", (void *)&char2 - (void *)&int2);
     printf("%p\n", (void *)&floats);
     printf("%p\n", (void *)&floats[0]);
     printf("%p\n", (void *)&floats[1]);
