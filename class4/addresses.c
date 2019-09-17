f6a363c51148e6f9c78f62762711af762df6b8f5
Blake Karwoski <bkarw@umich.edu>
2019-09-17 14:50:47 -0400

    Agreg

--- class4/addresses.c
+++ class4/addresses.c
@@ -18,7 +18,7 @@ int main(void) {
     printf("%p\n", (void *)&doubles[0]);
     printf("%p\n", (void *)&doubles[1]);
     printf("%p\n", (void *)&doubles[2]);
-    printf("%p\n", doubles[1] - doubles[0]);
+    printf("%f\n", doubles[1] - doubles[0]);
     printf("%ld\n", (long)doubles[1] - (long)doubles[0]);
 
     printf("%p\n", (void *)&char2);
