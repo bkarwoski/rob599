8f869343f48a9666ad4789ddaf378b87b809f9b5
Blake Karwoski <bkarw@umich.edu>
2019-09-17 13:38:19 -0400

    address distance

--- class4/addresses.c
+++ class4/addresses.c
@@ -1,4 +1,16 @@
-int main(){
+#include <stdio.h>
 
+char char1;
+int int1;
+
+
+int main(void){
+
+   // int variable = 10;
+   // printf("%p\n", (void *)&variable);
+   // return 0;
+    printf("%p\n", (void *)&char1);
+    printf("%p\n", (void *)&int1);
+    printf("%p\n", (void *)&char1-(void *)&int1);
 return 0;
 }
