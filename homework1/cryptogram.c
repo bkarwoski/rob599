074a168efffd2db34f8d7334bdbb647112077277
Blake Karwoski <bkarw@umich.edu>
2019-09-18 00:20:38 -0400

    error handling

--- homework1/cryptogram.c
+++ homework1/cryptogram.c
@@ -2,17 +2,14 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    if (argc == 1){
+    if (argc == 1) {
         fprintf(stderr, "usage: ./cryptogram <encrypt|decrypt> <password> <text>\n");
         return 1;
     }
-    if (strcmp(argv[1], "encrypt")==0){
-
+    if (strcmp(argv[1], "encrypt") == 0) {
     }
-    if (strcmp(argv[1], "decrypt")==0){
-
-    }
-    else {
+    if (strcmp(argv[1], "decrypt") == 0) {
+    } else {
         fprintf(stderr, "expected command encrypt or decrypt.\n");
         return 1;
     }
