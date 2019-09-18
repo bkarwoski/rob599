b826d4d7e9069e8718ad653c1ed7e87d73fd7162
Blake Karwoski <bkarw@umich.edu>
2019-09-18 09:48:59 -0400

    prog check

--- homework1/cryptogram.c
+++ homework1/cryptogram.c
@@ -7,12 +7,25 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     if (strcmp(argv[1], "encrypt") == 0) {
-    }
-    if (strcmp(argv[1], "decrypt") == 0) {
+
+    } else if (strcmp(argv[1], "decrypt") == 0) {
+
     } else {
         fprintf(stderr, "expected command encrypt or decrypt.\n");
         return 1;
     }
+    char fpass[100];
+    int j = 0;
+    for (int i = 0; argv[3][i] != '\0'; i++) {
+        if ((argv[3][i] >= 'a') && (argv[3][i] <= 'z')) {
+            fpass[j] = argv[3][i];
+            j++;
+        }
+        if ((argv[3][i] >= 'A') && (argv[3][i] <= 'Z')) {
+            fpass[j] = (argv[3][i] - 'A' + 'a');
+            j++;
+        }
+    }
 
-    return 0;
+
 }
