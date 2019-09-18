8a3158ef1319971e380cd9483dd141753b37f1c7
Blake Karwoski <bkarw@umich.edu>
2019-09-17 23:54:04 -0400

    argc

--- homework1/cryptogram.c
+++ homework1/cryptogram.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
-    if (argc ==1){
+    if (argc == 1){
+        fprintf(stderr, "usage: ./cryptogram <encrypt|decrypt> <password> <text>\n");
         return 1;
     }
 
