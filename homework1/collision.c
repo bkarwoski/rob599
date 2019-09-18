7fce2946de6a0ed03e82b9741d94f9e9d4a01ce8
Blake Karwoski <bkarw@umich.edu>
2019-09-18 11:43:48 -0400

    basic collision test

--- homework1/collision.c
+++ homework1/collision.c
@@ -31,10 +31,10 @@ void shift(poly_t *polyX) {
     }
 }
 
-void intersects(double x1, double y1, double x2, double y2,
+bool intersects(double x1, double y1, double x2, double y2,
                 double x3, double y3, double x4, double y4) {
     
-    //  return false;
+    return false;
 }
 
 //bool contains(double x1, double y1) {
