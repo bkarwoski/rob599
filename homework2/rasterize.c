410a5b5e0908ec28ce8b918d07da5c648891a576
Blake Karwoski <bkarw@umich.edu>
2019-10-06 18:24:30 -0400

    case 6?

--- homework2/rasterize.c
+++ homework2/rasterize.c
@@ -200,6 +200,47 @@ void case_5(void) {
     free(serialized_bmp);
 }
 
+void case_6(void) {
+    // draw a 600 (width – 40) by 440 (height – 40)
+    // white border centered at the center of the image (640 / 2, 480 / 2)
+    vector_xy_t sqPoints = vector_create();
+    int width = 600;
+    int height = 400;
+    vector_append(&sqPoints, width / 2, -1 * height/2);
+    vector_append(&sqPoints, width / 2, height / 2);
+    vector_append(&sqPoints, -1 * width / 2, height / 2);
+    vector_append(&sqPoints, -1 * width / 2, -1 * height / 2);
+
+    roundC(&sqPoints);
+
+    bitmap_t bmp = {0};
+    bmp.width = 640;
+    bmp.height = 480;
+    bmp.data = calloc(bmp.width * bmp.height, sizeof(color_bgr_t));
+    size_t bmp_size = bmp_calculate_size(&bmp);
+    uint8_t *serialized_bmp = malloc(bmp_size);
+    color_bgr_t white = {255, 255, 255};
+
+    vector_xy_t perimeter = vector_create();
+    gx_perimeter(&sqPoints, &perimeter);
+    gx_trans(640/2, 480/2, &perimeter, &perimeter);
+    gx_draw(&bmp, white, &perimeter);
+
+    bmp_serialize(&bmp, serialized_bmp);
+    FILE *f = fopen("my_image.bmp", "wb");
+    fwrite(serialized_bmp, bmp_size, 1, f);
+    fclose(f);
+
+    image_server_set_data(bmp_size, serialized_bmp);
+    image_server_start("8000");
+    sleep(1);
+
+    vector_delete(&sqPoints);
+    vector_delete(&perimeter);
+    free(bmp.data);
+    free(serialized_bmp);
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("usage: ./rasterize <test case>\n");
@@ -226,10 +267,10 @@ int main(int argc, char *argv[]) {
         case_5();
         return 0;
     }
-    /*if (i == 6) {
+    if (i == 6) {
         case_6();
         return 0;
-    }
+    }/*
     if (i == 7) {
         case_7();
         return 0;
