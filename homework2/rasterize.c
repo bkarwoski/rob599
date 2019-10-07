12118819c17f3d407fa2607c36209723509cf6dc
Blake Karwoski <bkarw@umich.edu>
2019-10-07 13:00:41 -0400

    case 5

--- homework2/rasterize.c
+++ homework2/rasterize.c
@@ -9,101 +9,6 @@
 #include "vector_xy_i32_t.h"
 
 /*
-void case_3(void) {
-    //draw a 4 by 4 pixel white square centered at (0, 0)
-    vector_xy_t coords = vector_create();
-
-    vector_append(&coords, 2, -2);
-    vector_append(&coords, 2, 2);
-    vector_append(&coords, -2, 2);
-    vector_append(&coords, -2, -2);
-
-    roundC(&coords);
-    //printf("coords.xData[0] = %f\n", coords.xData[0]);
-
-    bitmap_t bmp = {0};
-    bmp.width = 640;
-    bmp.height = 480;
-    bmp.data = calloc(bmp.width * bmp.height, sizeof(color_bgr_t));
-    size_t bmp_size = bmp_calculate_size(&bmp);
-    uint8_t *serialized_bmp = malloc(bmp_size);
-    color_bgr_t white = {255, 255, 255};
-
-    vector_xy_t perimeter = vector_create();
-    int x0 = 0;
-    int y0 = 0;
-    int x1 = 0;
-    int y1 = 0;
-    for (int i = 0; i < coords.size; i++) {
-        x0 = coords.xData[i];
-        y0 = coords.yData[i];
-        x1 = coords.xData[(i + 1) % coords.size];
-        y1 = coords.yData[(i + 1) % coords.size];
-        gx_rasterize_line(x0, y0, x1, y1, &perimeter);
-    }
-    
-    printf("coords.xData[3]: %f\n", coords.xData[3]);
-    printf("coords.yData[3]: %f\n", coords.yData[3]);
-    gx_draw(&bmp, white, &perimeter);
-
-    bmp_serialize(&bmp, serialized_bmp);
-    FILE *f = fopen("my_image.bmp", "wb");
-    fwrite(serialized_bmp, bmp_size, 1, f);
-    fclose(f);
-
-    image_server_set_data(bmp_size, serialized_bmp);
-    image_server_start("8000");
-    sleep(1);
-
-    vector_delete(&coords);
-    vector_delete(&perimeter);
-    free(bmp.data);
-    free(serialized_bmp);
-}
-
-void case_4(void) {
-    //draw a 4 by 4 pixel white square centered at (2, 2)
-    vector_xy_t coords = vector_create();
-
-    vector_append(&coords, 2, -2);
-    vector_append(&coords, 2, 2);
-    vector_append(&coords, -2, 2);
-    vector_append(&coords, -2, -2);
-
-    roundC(&coords);
-
-    bitmap_t bmp = {0};
-    bmp.width = 640;
-    bmp.height = 480;
-    bmp.data = calloc(bmp.width * bmp.height, sizeof(color_bgr_t));
-    size_t bmp_size = bmp_calculate_size(&bmp);
-    uint8_t *serialized_bmp = malloc(bmp_size);
-    color_bgr_t white = {255, 255, 255};
-
-    vector_xy_t perimeter = vector_create();
-    gx_perimeter(&coords, &perimeter);
-    //gx_trans(double x, double y, vector_xy_t *orig, vector_xy_t *shifted)
-    gx_trans(2, 2, &perimeter, &perimeter);
-    
-    printf("coords.xData[3]: %f\n", coords.xData[3]);
-    printf("coords.yData[3]: %f\n", coords.yData[3]);
-    gx_draw(&bmp, white, &perimeter);
-
-    bmp_serialize(&bmp, serialized_bmp);
-    FILE *f = fopen("my_image.bmp", "wb");
-    fwrite(serialized_bmp, bmp_size, 1, f);
-    fclose(f);
-
-    image_server_set_data(bmp_size, serialized_bmp);
-    image_server_start("8000");
-    sleep(1);
-
-    vector_delete(&coords);
-    vector_delete(&perimeter);
-    free(bmp.data);
-    free(serialized_bmp);
-}
-
 void case_5(void) {
     // draw a 5 by 5 pixel white square centered at (2, 2)
     vector_xy_t coords = vector_create();
@@ -285,6 +190,12 @@ int main(int argc, char *argv[]) {
         vector_delete(&rect);
     } else if (caseNum == 4) {
         vector_xy_t rect = gx_rect(4, 4);
+        gx_trans(2, 2, &rect);
+        roundC(&rect);
+        gx_draw_poly(&bmp, white, &rect);
+        vector_delete(&rect);
+    } else if (caseNum == 5) {
+        vector_xy_t rect = gx_rect(5, 5);
         gx_trans(2, 2, &rect);
         roundC(&rect);
         gx_draw_poly(&bmp, white, &rect);
