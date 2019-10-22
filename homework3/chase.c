057adf3a4a99e5e159ad2a2423ac4a08123a5125
Blake Karwoski <bkarw@umich.edu>
2019-10-22 15:30:26 -0400

    progress save

--- homework3/chase.c
+++ homework3/chase.c
@@ -9,6 +9,7 @@
 #include "bmp.h"
 #include "graphics.h"
 #include "image_server.h"
+#include "collision.h"
 #define M_PI 3.14159265358979323846
 #define WIDTH 640
 #define HEIGHT 480
@@ -48,9 +49,7 @@ typedef struct state {
     bitmap_t bmp;
     size_t image_size;
     uint8_t *image_data;
-
     bool runner_caught;
-
     agent_t runner;
     agent_t chaser;
 } state_t;
@@ -61,21 +60,81 @@ void drawMap(bitmap_t *bmp) {
     color_bgr_t white = {255, 255, 255};
     for(int i = 0; i < bmp->width * bmp->height; i++){
         bmp->data[i] = white;
-        //printf("i = %d\n", i);
     }
     for (int i = 0; i < MAP_W * MAP_H; i++) {
         if (MAP[i] == 'X') {
             vector_xy_t nextBlock = gx_rect(BLOCK_SIZE, BLOCK_SIZE);
             double xPos = BLOCK_SIZE / 2 + (i % MAP_W) * BLOCK_SIZE;
-            printf("xPos = %f\n", xPos);
             double yPos = BLOCK_SIZE / 2 + ((i - i % MAP_W) / MAP_W) * BLOCK_SIZE;
-            printf("yPos = %f\n", yPos);
             gx_trans(xPos, yPos, &nextBlock);
             gx_fill_poly(bmp, black, &nextBlock);
             vector_delete(&nextBlock);
         }
     }
+}
 
+void updateGraphics(state_t *state) {
+    color_bgr_t red = {0, 0, 255};
+    color_bgr_t green = {0, 255, 0};
+    drawMap(&state->bmp);
+    vector_xy_t runPoints = gx_rob();
+    gx_trans(state->runner.x, state->runner.y, &runPoints);
+    gx_rot(state->runner.theta, &runPoints);
+    gx_fill_poly(&state->bmp, green, &runPoints);
+    vector_delete(&runPoints);
+    vector_xy_t chasePoints = gx_rob();
+    gx_trans(state->chaser.x, state->chaser.y, &chasePoints);
+    gx_rot(state->chaser.theta, &chasePoints);
+    gx_fill_poly(&state->bmp, red, &chasePoints);
+    vector_delete(&chasePoints);
+}
+
+void handleCollisions(state_t *state) {
+
+}
+
+int runnerAction(void) {
+    int action = rand() % 20;
+    if (action == 2 || action == 3) {
+        return action;
+    }
+    return 1;
+}
+
+int chaserAction(void) {
+    return 2;
+}
+
+void applyAction(agent_t *bot, int action) {
+    if (action == 2) {
+        bot->vel += 2;
+        if (bot->vel > 12) {
+            bot->vel = 12;
+        }
+    }
+    if (action == 3) {
+        bot->ang_vel += M_PI / 16;
+    }
+    if (action == 4) {
+        bot->ang_vel -= M_PI / 16;
+    }
+}
+
+bool resolveCollisions(agent_t *bot) {
+
+    return false;
+}
+
+void moveBot(agent_t *bot, int action) {
+    applyAction(bot, action);
+    bot->theta += bot->ang_vel;
+    bot->ang_vel *= 0.8;
+    bot->x += bot->vel * cos(bot->theta);
+    bot->y += bot->vel * -sin(bot->theta);
+    //resolveCollisions, and return bool for collides
+    if (resolveCollisions(bot)) {
+        bot->vel *= 0.25;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -87,25 +146,36 @@ int main(int argc, char *argv[]) {
     long nanoseconds = 40 * 1000 * 1000;
     struct timespec interval = { seconds, nanoseconds };
     int fast = atoi(argv[2]);
+    int runnerIndex = atoi(argv[3]);
+    int chaserIndex = 97;
     state_t state;
     state.time_step = atoi(argv[1]);
-    bitmap_t bmp = {0};
-    bmp.width = WIDTH;
-    bmp.height = HEIGHT;
-    bmp.data = calloc(bmp.width * bmp.height, sizeof(color_bgr_t));
-    size_t bmp_size = bmp_calculate_size(&bmp);
-    uint8_t *serialized_bmp = malloc(bmp_size);
+    state.bmp.width = WIDTH;
+    state.bmp.height = HEIGHT;
+    state.bmp.data = calloc(state.bmp.width * state.bmp.height, sizeof(color_bgr_t));
+    state.image_size = bmp_calculate_size(&state.bmp);
+    state.image_data = malloc(state.image_size);
+    state.runner.x = BLOCK_SIZE / 2 + (runnerIndex % MAP_W) * BLOCK_SIZE;
+    state.runner.y = BLOCK_SIZE / 2 + ((runnerIndex - runnerIndex % MAP_W) / MAP_W) * BLOCK_SIZE;
+    state.runner.theta = 0;
+    state.runner.ang_vel = 0;
+    state.chaser.x = WIDTH / 2;
+    state.chaser.y = HEIGHT / 2;
+    state.chaser.theta = 0;
+    state.chaser.ang_vel = 0;
+
+
     image_server_start("8000");
     for (int i = 0; i < state.time_step; i++) {
         if (fast == 0) {
-            drawMap(&bmp);
-            bmp_serialize(&bmp, serialized_bmp);
-            image_server_set_data(bmp_size, serialized_bmp);
+            updateGraphics(&state);
+            bmp_serialize(&state.bmp, state.image_data);
+            image_server_set_data(state.image_size, state.image_data);
             nanosleep(&interval, NULL);
+            moveBot(&state.runner, runnerAction());
         }
-
     }
-    free(serialized_bmp); 
-    free(bmp.data);
+    free(state.image_data); 
+    free(state.bmp.data);
     return 0;
 }
