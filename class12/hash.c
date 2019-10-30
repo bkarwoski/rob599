dac1759d5751c8f34fcf8ecede11bf98bc38064a
Blake Karwoski <bkarw@umich.edu>
2019-10-30 14:35:17 -0400

    add

--- class12/hash.c
+++ class12/hash.c
@@ -1,5 +1,23 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdint.h>
+#define TABLE_SIZE 8192
 
+uint32_t add_hash(uint8_t *data, int n) {
+    uint32_t hash = 0;
+    for (int i = 0; i < n; i++) {
+        hash += *data;
+        data++;
+    }
+    return hash;
+}
 
 int main(int argc, char **argv) {
+    //char *hashFun = argv[1];
+    uint8_t *input = (uint8_t*)argv[2];
+    printf("0x%x\n", add_hash(input, strlen(argv[2])));
     return 0;
 }
