22fbf48a00c6efa7f53e289334ddfc10f9d75b88
Blake Karwoski <bkarw@umich.edu>
2019-11-06 14:06:14 -0500

    init commit

---
+++ class14/helloworlds.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#define N_THREADS 16
+#include <stdio.h>
+
+typedef struct thread_info {
+    int num;
+    pthread_t thread;
+} thread_info_t;
+
+void *thread_start(void *user) {
+    thread_info_t *info = user;
+    printf("Hello world from %d!\n", info->num);
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+thread_info_t thread_infos[N_THREADS];
+for (int i = 0; i < N_THREADS; i++) {
+    thread_infos[i].num = i;
+    pthread_create(&thread_infos[i].thread, NULL, thread_start, &thread_infos[i]);
+    pthread_join(thread_infos[i].thread, NULL);
+}
+    return 0;
+}
