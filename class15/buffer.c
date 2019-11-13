37e2085b0f5b9eee600c6584524c44eb0281f6cb
Blake Karwoski <bkarw@umich.edu>
2019-11-13 14:40:50 -0500

    mutex first try

--- class15/buffer.c
+++ class15/buffer.c
@@ -10,6 +10,8 @@
 int buffer[BUFFER_CAPACITY];
 int buffer_size = 0;
 bool producerFinished = false;
+bool usingMutex = false;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 struct timespec interval = {0, 1};
 
@@ -20,8 +22,14 @@ void *producer_thread(void *user) {
     int i = 0;
     while (i < 30) {
         if (buffer_size < 8) {
+            if (usingMutex) {
+                pthread_mutex_lock(&mutex);
+            }
             buffer[buffer_size] = i;
             buffer_size++;
+            if (usingMutex) {
+                pthread_mutex_unlock(&mutex);
+            }
             i++;
         } else {
             nanosleep(&interval, NULL);
@@ -32,13 +40,23 @@ void *producer_thread(void *user) {
 }
 
 int main(int argc, char *argv[]) {
+    if (argc == 2) {
+        usingMutex = true;
+    }
     pthread_t producer;
     pthread_create(&producer, NULL, producer_thread, NULL);
     //consumer thread
     while (!producerFinished) {
         if (buffer_size > 0) {
-            printf("%d\n", buffer[buffer_size-1]);
+            if (usingMutex) {
+                pthread_mutex_lock(&mutex);
+            }
             buffer_size--;
+            int toPrint = buffer[buffer_size];
+            if (usingMutex) {
+                pthread_mutex_unlock(&mutex);
+            }
+            printf("%d\n", toPrint);
         }
     }
     pthread_join(producer, NULL);
