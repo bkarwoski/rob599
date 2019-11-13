3eb8c6860bfbcd8ea0852348610c78d1e2d800e2
Blake Karwoski <bkarw@umich.edu>
2019-11-13 16:02:48 -0500

    fgetc

--- class16/chase.c
+++ class16/chase.c
@@ -7,6 +7,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <float.h>
+#include <pthread.h>
+#include <termios.h>
 #include "bmp.h"
 #include "graphics.h"
 #include "image_server.h"
@@ -30,6 +32,10 @@
 #define MAP_W (1.0 * WIDTH / BLOCK_SIZE)
 #define MAP_H (1.0 * HEIGHT / BLOCK_SIZE)
 #define MAX_DEPTH 4
+struct termios original_termios;
+void reset_terminal(void) {
+    tcsetattr(STDIN_FILENO, TCSANOW, &original_termios);
+}
 
 typedef struct agent {
     bool is_runner;
@@ -55,6 +61,32 @@ typedef struct state {
     agent_t runner;
     agent_t chaser;
 } state_t;
+
+void *io_thread(void *user) {
+    state_t *state = user;
+
+    // we need to return the terminal to its original state when our program quits
+    // if we don't the terminal will not behave correctly
+    tcgetattr(0, &original_termios);
+    atexit(reset_terminal);
+
+    // we start with the old settings, since we are only changing a few things
+    struct termios new_termios;
+    memcpy(&new_termios, &original_termios, sizeof(new_termios));
+
+    // we turn off the bits for echo (show typed characters on the screen)
+    // and canonical mode, which waits until you press newline to give the program input
+    new_termios.c_lflag &= ~(ECHO | ICANON);
+    // we are modifying the input settings, (standard in, stdin)
+    // and we want the changes to take effect right now!
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+    while (true) {
+        // put the rest of your code here!
+        // read characters with getc(stdin)
+        int c = getc(stdin);
+        printf("%c: %d\n", c, c);
+    }
+}
 
 void drawMap(bitmap_t *bmp) {
     color_bgr_t black = {0, 0, 0};
@@ -98,7 +130,7 @@ int runnerAction(void) {
         return action;
     }
     action = 0;
-    printf("%d ", action);
+    //printf("%d ", action);
     return action;
 }
 
@@ -249,6 +281,9 @@ int main(int argc, char *argv[]) {
     if (fast == 0) {
         image_server_start("8000");
     }
+
+    pthread_t userInput;
+    pthread_create(&userInput, NULL, io_thread, &userInput);
     for (int i = 0; i < state.time_step; i++) {
         if (fast == 0) {
             updateGraphics(&state);
@@ -265,7 +300,7 @@ int main(int argc, char *argv[]) {
         //search_actions(search_node, &chosen_action);
         moveBot(&state.runner, runnerAction());
         moveBot(&state.chaser, chosen_action);
-        printf("%d\n", chosen_action);
+        //printf("%d\n", chosen_action);
         if (robCollision(state.runner, state.chaser)) {
             exit(0);
         }
