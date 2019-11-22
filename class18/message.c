f741255e90f5cc63e6cfd941768f4e3ee0db483d
Blake Karwoski <bkarw@umich.edu>
2019-11-22 17:17:15 -0500

    prints

--- class18/message.c
+++ class18/message.c
@@ -18,17 +18,15 @@ double seconds_now(void) {
 }
 
 int main(void) {
-    // we could also pass in a string of settings for special LCM configuration
-    // and that would be helpful to configure a multi-robot communication scenario
     lcm_t *lcm = lcm_create(NULL);
     l2g_t message = {0};
     message.utime = 0;
     message.l2g[0] = 1.0;
     message.l2g[1] = 2.0;
     message.l2g[2] = 3.0;
-    l2g_t_publish(lcm, "L2G", &message);
-    l2g_t_subscription_t *l2g_sub = l2g_t_subscribe(lcm, "L2G", on_l2g, &message);
+    l2g_t_subscription_t *l2g_sub = l2g_t_subscribe(lcm, "L2G", on_l2g, NULL);
     double start = seconds_now();
+    l2g_t_publish(lcm, "L2G", &message);
     while (seconds_now() - start < 0.5) {
         lcm_handle_timeout(lcm, 100);
     }
